01de77706dbeb79068576f788b7b572528ae58b0
ZiganshinIB <elzig3012@gmail.com>
2023-11-24 13:57:22 +0300

    Refract

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -125,6 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'djangofortest777@gmail.com'
-EMAIL_HOST_PASSWORD = 'gqebztnucumfaoes'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
