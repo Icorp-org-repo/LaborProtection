6ad8208de7c688036eff40b11954275e126fd73e
ZiganshinIB <elzig3012@gmail.com>
2023-11-25 20:29:50 +0300

    Ref

--- mysite/elprotection/templatetags/elprotection_tags.py
+++ mysite/elprotection/templatetags/elprotection_tags.py
@@ -3,3 +3,8 @@ from django.db.models import Count
 from ..models import Protocol
 
 register = template.Library()
+
+@register.inclusion_tag('elprotection/include/sidebar.html')
+def show_categories(sort=None):
+
+    return {"cats": cats, 'cat_selected': cat_selected}
