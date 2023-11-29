73744e5b9aab262e0051b383f8e4c70bb0f9001d
ZiganshinIB <elzig3012@gmail.com>
2023-11-29 15:59:16 +0300

    Add forms

--- mysite/elprotection/forms.py
+++ mysite/elprotection/forms.py
@@ -1,7 +1,37 @@
 from django import forms
-
+from .models import Employ, Position, Company, Admission, Protocol
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Логин")
     password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ['title',]
+
+class PositionForm(forms.ModelForm):
+    class Meta:
+        model = Position
+        fields = ['title', 'company', 'category',]
+
+
+class EmployForm(forms.ModelForm):
+    class Meta:
+        model = Employ
+        fields = ['number','second_name', 'name', 'surname', 'position', 'boss', ]
+
+
+class AdmissionForm(forms.ModelForm):
+    class Meta:
+        model = Admission
+        fields = ['number', 'title', 'body', 'knowledge', 'type']
+
+
+class ProtocolForm(forms.ModelForm):
+    class Meta:
+        model = Protocol
+        fields = ['status', 'employ', 'admission', 'start', 'numbers', 'end']
+        
 
